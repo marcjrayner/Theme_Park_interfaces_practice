cd0d0b3d223ac77309bac7c964f4b7d0acda6f7e
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-14 20:55:25 +0000

    implements ITicketed in dodgems

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(11, 123.5, 20.50);
+        visitor2 = new Visitor(18, 163.5, 200.50);
     }
 
 
@@ -28,5 +33,20 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void canGetDefaultPrice() {
+        assertEquals(4.50, dodgems.defaultPrice(), 0.1);
+    }
+
+    @Test
+    public void canGetHalfPriceIfVisitorUnderAge12() {
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.1);
+    }
+
+    @Test
+    public void doesNotHalfPriceIfVisitorOverAge12() {
+        assertEquals(4.50, dodgems.priceFor(visitor2), 0.1);
     }
 }
